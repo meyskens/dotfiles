66b5e3cbc9aa0d685be8958470d160b0f90ad89b
Maartje Eyskens <meyskens@me.com>
2018-04-22 17:16:19 +0100

    rm helm helper

--- .config/fish/functions/kubernetes.fish
+++ .config/fish/functions/kubernetes.fish
@@ -10,7 +10,5 @@ alias kn='kubectl get nodes'
 alias kp='kubectl get pods'
 alias kcc='bash -c "kubectl config set-context \$0 --namespace=\$1 && kubectl config use-context \$0"'
 
-alias helm='TILLER_NAMESPACE="(_k_currentNamespace)-tiller" /usr/local/bin/helm'
-
 # Helper aliases
 alias _k_currentNamespace='kubectl config view -o json | jq -r ".contexts | map(select(.name == \"(kubectl config current-context)\")) | .[0].context.namespace"'
